2d05ba064a8981ea1dfc33a4729ecd1e5723591f
Ana Batinovic <ana.batinovic.ab@gmail.com>
2023-04-04 10:06:46 +0200

    Add SegmentationNameMaskArray msg, remove filtering from mask, publish on topic

--- scripts/create_mask.py
+++ scripts/create_mask.py
@@ -7,8 +7,8 @@ import cv2
 import numpy as np
 from color_coding_dict import *
 import time
-from semantic_segmentation_ros.msg import SegmentationNameMask
-
+from semantic_segmentation_ros.msg import SegmentationNameMask, SegmentationNameMaskArray
+import copy
 
 class CreateMask:
 
@@ -16,35 +16,43 @@ class CreateMask:
         # Instantiate CvBridge
         self.bridge = CvBridge()
         
-        self.input_segm_sub = rospy.Subscriber("segmentation/color/image_raw", Image, self.image_callback, queue_size=1)
-        self.mask_image_pub = rospy.Publisher("segmentation/mask/image_raw", Image, queue_size=1)
-        self.label_image_pub = rospy.Publisher("segmentation/label/image_raw", Image, queue_size=1)
-        self.segmentation_mask_pub = rospy.Publisher("segmentation/mask", SegmentationNameMask, queue_size=1)
+        self.mask_image_pub = rospy.Publisher(
+            "segmentation/mask/image_raw", Image, queue_size=1)
+        self.label_image_pub = rospy.Publisher(
+            "segmentation/label/image_raw", Image, queue_size=1)
+        self.segmentation_mask_pub = rospy.Publisher(
+            "segmentation/mask", SegmentationNameMaskArray, queue_size=1)
+        self.input_segm_sub = rospy.Subscriber(
+            "segmentation/color/image_raw", Image, self.image_callback, queue_size=1)
 
         self.segmentation_name_mask = SegmentationNameMask()
+        self.segmentation_name_mask_array = SegmentationNameMaskArray()
 
     def image_callback(self, msg):
+        self.segmentation_name_mask_array.masks = []
         try:
             # Convert your ROS Image message to OpenCV2
             cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-            object_name, object_mask, label_image = self.get_mask_and_name(cv2_img)
-            object_mask_img = self.bridge.cv2_to_imgmsg(object_mask, "bgr8")
-            label_image = self.bridge.cv2_to_imgmsg(label_image, "bgr8")
+            object_name_array, object_mask_array, label_image_array = self.get_mask_and_name(cv2_img)
+            for i in range (0, len(object_mask_array)):
+                object_mask_img = self.bridge.cv2_to_imgmsg(object_mask_array[i], "bgr8")
+                label_image = self.bridge.cv2_to_imgmsg(label_image_array[i], "bgr8")
+                self.label_image_pub.publish(label_image)
+                self.mask_image_pub.publish(object_mask_img)
+                # Fill out msg and publish
+                self.segmentation_name_mask.name = object_name_array[i]
+                self.segmentation_name_mask.mask = object_mask_img
+                self.segmentation_name_mask_array.masks.append(copy.deepcopy(self.segmentation_name_mask))       
+            self.segmentation_mask_pub.publish(self.segmentation_name_mask_array)
         except CvBridgeError as e:
             print(e)
-        else:
-            self.mask_image_pub.publish(object_mask_img)
-            self.label_image_pub.publish(label_image)
-            # Fill out msg and publish
-            self.segmentation_name_mask.name = object_name
-            self.segmentation_name_mask.mask = object_mask_img            
-            self.segmentation_mask_pub.publish(self.segmentation_name_mask)
-
-
+    
     def get_mask_and_name(self, img):
         # Convert to hsv colorspace
         hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
+        mask_img_object_array = []
+        hsv_image_array = []
+        name_array = []
         for name in color_dict.keys():
             # Get colors from color coding dict - object name, segmentation color
             r, g, b = color_dict[name]
@@ -54,14 +62,10 @@ class CreateMask:
 
             # Find the colors within the boundaries, mask with all objects
             mask_img = cv2.inRange(hsv_img, lower_bound, upper_bound)
-            # Define kernel size, filter
-            kernel = np.ones((10, 10), np.uint8)
-            # Remove unnecessary noise from mask
-            mask_img = cv2.morphologyEx(mask_img, cv2.MORPH_CLOSE, kernel)
-            mask_img = cv2.morphologyEx(mask_img, cv2.MORPH_OPEN, kernel)
 
             # Segment only the detected region
             _, contours, hierarchy = cv2.findContours(mask_img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            
             
             # Create mask for each object and name, publish it
             for contour in contours:
@@ -79,9 +83,14 @@ class CreateMask:
                     # Reaching pixels [y, x]
                     cv2.putText(hsv_img, "{}".format(name, cY, cX, 15),
                         (cX - 40, cY - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
-
-                # Return name and mask to be published
-                return name, mask_img_object, hsv_img, 
+                
+                    # cv2.imshow('image1',mask_img_object)
+                    # cv2.waitKey(0)
+                    mask_img_object_array.append(mask_img_object)
+                    hsv_image_array.append(hsv_img)
+                    name_array.append(name)                
+        # Return name and mask to be published
+        return name_array, mask_img_object_array, hsv_image_array
 
 if __name__ == '__main__':
     rospy.init_node('create_mask')
